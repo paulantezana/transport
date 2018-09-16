44652c70b556ad73e5c320ca03eb650af7ce8bb1
paulantezana <contacto@paulantezana.com>
2018-09-15 19:28:29 -0500

    new updates

---
+++ controller/category.go
@@ -0,0 +1,187 @@
+package controller
+
+import (
+    "fmt"
+    "github.com/labstack/echo"
+    "github.com/paulantezana/transport/config"
+    "github.com/paulantezana/transport/models"
+    "github.com/paulantezana/transport/utilities"
+    "net/http"
+)
+func GetCategories(c echo.Context) error {
+    // Get data request
+    request := utilities.Request{}
+    if err := c.Bind(&request); err != nil {
+        return err
+    }
+
+    // Get connection
+    db := config.GetConnection()
+    defer db.Close()
+
+    // Pagination calculate
+    if request.CurrentPage == 0 {
+        request.CurrentPage = 1
+    }
+    offset := request.Limit*request.CurrentPage - request.Limit
+
+    // Check the number of matches
+    var total uint
+    categories := make([]models.Category, 0)
+
+    // Find categories
+    if err := db.Where("lower(name) LIKE lower(?)", "%"+request.Search+"%").
+        Order("id desc").
+        Offset(offset).Limit(request.Limit).Find(&categories).
+        Offset(-1).Limit(-1).Count(&total).
+        Error; err != nil {
+        return err
+    }
+
+    // Type response
+    // 0 = all data
+    // 1 = minimal data
+    if request.Type == 1 {
+        customCategories := make([]models.Category, 0)
+        for _, category := range categories {
+            customCategories = append(customCategories, models.Category{
+                ID:   category.ID,
+                Name: category.Name,
+            })
+        }
+        return c.JSON(http.StatusCreated, utilities.ResponsePaginate{
+            Success:     true,
+            Data:        customCategories,
+            Total:       total,
+            CurrentPage: request.CurrentPage,
+        })
+    }
+    // Return response
+    return c.JSON(http.StatusCreated, utilities.ResponsePaginate{
+        Success:     true,
+        Data:        categories,
+        Total:       total,
+        CurrentPage: request.CurrentPage,
+    })
+}
+
+func GetCategoryByID(c echo.Context) error {
+    // Get data request
+    category := models.Category{}
+    if err := c.Bind(&category); err != nil {
+        return err
+    }
+
+    // Get connection
+    db := config.GetConnection()
+    defer db.Close()
+
+    // Execute instructions
+    if err := db.First(&category, category.ID).Error; err != nil {
+        return err
+    }
+
+    // Return response
+    return c.JSON(http.StatusCreated, utilities.Response{
+        Success: true,
+        Data:    category,
+    })
+}
+
+func CreateCategory(c echo.Context) error {
+    // Get data request
+    category := models.Category{}
+    if err := c.Bind(&category); err != nil {
+        return err
+    }
+
+    // get connection
+    db := config.GetConnection()
+    defer db.Close()
+
+    // Insert category in database
+    if err := db.Create(&category).Error; err != nil {
+        return c.JSON(http.StatusOK, utilities.Response{
+            Message: fmt.Sprintf("%s", err),
+        })
+    }
+
+    // Return response
+    return c.JSON(http.StatusCreated, utilities.Response{
+        Success: true,
+        Data:    category.ID,
+        Message: fmt.Sprintf("El usuario %s se registro exitosamente", category.Name),
+    })
+}
+
+func UpdateCategory(c echo.Context) error {
+    // Get data request
+    newCategory := models.Category{}
+    if err := c.Bind(&newCategory); err != nil {
+        return err
+    }
+    oldCategory := models.Category{
+        ID: newCategory.ID,
+    }
+
+    // get connection
+    db := config.GetConnection()
+    defer db.Close()
+
+    // Validation category exist
+    if db.First(&oldCategory).RecordNotFound() {
+        return c.JSON(http.StatusOK, utilities.Response{
+            Message: fmt.Sprintf("No se encontró el registro con id %d", oldCategory.ID),
+        })
+    }
+
+    // Update category in database
+    if err := db.Model(&newCategory).Update(newCategory).Error; err != nil {
+        return err
+    }
+    if !newCategory.State {
+        if err := db.Model(newCategory).UpdateColumn("state", false).Error; err != nil {
+            return err
+        }
+    }
+
+    // Return response
+    return c.JSON(http.StatusOK, utilities.Response{
+        Success: true,
+        Data:    newCategory.ID,
+        Message: fmt.Sprintf("Los datos del usuario %s, se actualizarón correctamente", oldCategory.Name),
+    })
+}
+
+func DeleteCategory(c echo.Context) error {
+    // Get data request
+    category := models.Category{}
+    if err := c.Bind(&category); err != nil {
+        return err
+    }
+
+    // get connection
+    db := config.GetConnection()
+    defer db.Close()
+
+    // Validation category exist
+    if db.First(&category).RecordNotFound() {
+        return c.JSON(http.StatusOK, utilities.Response{
+            Message: fmt.Sprintf("No se encontró el registro con id %d", category.ID),
+        })
+    }
+
+    // Delete category in database
+    if err := db.Delete(&category).Error; err != nil {
+        return c.JSON(http.StatusOK, utilities.Response{
+            Message: fmt.Sprintf("%s", err),
+        })
+    }
+
+    // Return response
+    return c.JSON(http.StatusOK, utilities.Response{
+        Success: true,
+        Data:    category.ID,
+        Message: fmt.Sprintf("El usuario %s, se elimino correctamente", category.Name),
+    })
+}
---
+++ controller/company.go
@@ -0,0 +1,188 @@
+package controller
+
+import (
+    "fmt"
+    "github.com/labstack/echo"
+    "github.com/paulantezana/transport/config"
+    "github.com/paulantezana/transport/models"
+    "github.com/paulantezana/transport/utilities"
+    "net/http"
+)
+
+func GetCompanies(c echo.Context) error {
+    // Get data request
+    request := utilities.Request{}
+    if err := c.Bind(&request); err != nil {
+        return err
+    }
+
+    // Get connection
+    db := config.GetConnection()
+    defer db.Close()
+
+    // Pagination calculate
+    if request.CurrentPage == 0 {
+        request.CurrentPage = 1
+    }
+    offset := request.Limit*request.CurrentPage - request.Limit
+
+    // Check the number of matches
+    var total uint
+    companies := make([]models.Company, 0)
+
+    // Find companies
+    if err := db.Where("lower(name) LIKE lower(?)", "%"+request.Search+"%").
+        Order("id desc").
+        Offset(offset).Limit(request.Limit).Find(&companies).
+        Offset(-1).Limit(-1).Count(&total).
+        Error; err != nil {
+        return err
+    }
+
+    // Type response
+    // 0 = all data
+    // 1 = minimal data
+    if request.Type == 1 {
+        customCompanies := make([]models.Company, 0)
+        for _, company := range companies {
+            customCompanies = append(customCompanies, models.Company{
+                ID:   company.ID,
+                Name: company.Name,
+            })
+        }
+        return c.JSON(http.StatusCreated, utilities.ResponsePaginate{
+            Success:     true,
+            Data:        customCompanies,
+            Total:       total,
+            CurrentPage: request.CurrentPage,
+        })
+    }
+    // Return response
+    return c.JSON(http.StatusCreated, utilities.ResponsePaginate{
+        Success:     true,
+        Data:        companies,
+        Total:       total,
+        CurrentPage: request.CurrentPage,
+    })
+}
+
+func GetCompanyByID(c echo.Context) error {
+    // Get data request
+    company := models.Company{}
+    if err := c.Bind(&company); err != nil {
+        return err
+    }
+
+    // Get connection
+    db := config.GetConnection()
+    defer db.Close()
+
+    // Execute instructions
+    if err := db.First(&company, company.ID).Error; err != nil {
+        return err
+    }
+
+    // Return response
+    return c.JSON(http.StatusCreated, utilities.Response{
+        Success: true,
+        Data:    company,
+    })
+}
+
+func CreateCompany(c echo.Context) error {
+    // Get data request
+    company := models.Company{}
+    if err := c.Bind(&company); err != nil {
+        return err
+    }
+
+    // get connection
+    db := config.GetConnection()
+    defer db.Close()
+
+    // Insert company in database
+    if err := db.Create(&company).Error; err != nil {
+        return c.JSON(http.StatusOK, utilities.Response{
+            Message: fmt.Sprintf("%s", err),
+        })
+    }
+
+    // Return response
+    return c.JSON(http.StatusCreated, utilities.Response{
+        Success: true,
+        Data:    company.ID,
+        Message: fmt.Sprintf("El usuario %s se registro exitosamente", company.Name),
+    })
+}
+
+func UpdateCompany(c echo.Context) error {
+    // Get data request
+    newCompany := models.Company{}
+    if err := c.Bind(&newCompany); err != nil {
+        return err
+    }
+    oldCompany := models.Company{
+        ID: newCompany.ID,
+    }
+
+    // get connection
+    db := config.GetConnection()
+    defer db.Close()
+
+    // Validation company exist
+    if db.First(&oldCompany).RecordNotFound() {
+        return c.JSON(http.StatusOK, utilities.Response{
+            Message: fmt.Sprintf("No se encontró el registro con id %d", oldCompany.ID),
+        })
+    }
+
+    // Update company in database
+    if err := db.Model(&newCompany).Update(newCompany).Error; err != nil {
+        return err
+    }
+    if !newCompany.State {
+        if err := db.Model(newCompany).UpdateColumn("state", false).Error; err != nil {
+            return err
+        }
+    }
+
+    // Return response
+    return c.JSON(http.StatusOK, utilities.Response{
+        Success: true,
+        Data:    newCompany.ID,
+        Message: fmt.Sprintf("Los datos del usuario %s, se actualizarón correctamente", oldCompany.Name),
+    })
+}
+
+func DeleteCompany(c echo.Context) error {
+    // Get data request
+    company := models.Company{}
+    if err := c.Bind(&company); err != nil {
+        return err
+    }
+
+    // get connection
+    db := config.GetConnection()
+    defer db.Close()
+
+    // Validation company exist
+    if db.First(&company).RecordNotFound() {
+        return c.JSON(http.StatusOK, utilities.Response{
+            Message: fmt.Sprintf("No se encontró el registro con id %d", company.ID),
+        })
+    }
+
+    // Delete company in database
+    if err := db.Delete(&company).Error; err != nil {
+        return c.JSON(http.StatusOK, utilities.Response{
+            Message: fmt.Sprintf("%s", err),
+        })
+    }
+
+    // Return response
+    return c.JSON(http.StatusOK, utilities.Response{
+        Success: true,
+        Data:    company.ID,
+        Message: fmt.Sprintf("El usuario %s, se elimino correctamente", company.Name),
+    })
+}
